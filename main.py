d13311689cb31834bd7c98fbf072ce874b17a601
zkilzkil <eskil.buvarp@gmail.com>
2025-06-07 00:50:14 +0200

    count words, update file

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("greetings boots")
-
+# read file
+def get_book_text(filepath):
+	with open(filepath) as f: 
+		filecontents = f.read()
+		return filecontents
+# func for number of words in file
+def count_words(text):
+	return len(text.split())
+# execute program
+def main():
+	text = get_book_text("books/frankenstein.txt")
+	num_words = count_words(text)
+	print(f"{num_words} words found in the document")
+#	print(get_book_text("books/frankenstein.txt"))
+main()
